1e5774fdf6182428df4a603a271e39a5dcea25f8
maxkim <max@maxkim.com>
2019-08-23 16:10:15 -0700

    practice log file parsing, fizz buzz continued

--- maxtest.py
+++ maxtest.py
@@ -3,18 +3,29 @@ import os
 
 log_dir = "data_dir"
 log_file = "log.txt"
+
 log_path = log_dir+"/"+log_file
-print (log_path)
 
 f = open(log_path,'r')
-count = 0
 
+count = 0
 for line in f:
     if re.search("^Aug",line):
         count += 1
+print ("lines with string \"Aug\": ",count)
+f.close()
 
-print (count)
+f = open(log_path,'r')
 
+count2 = 0
+for line in f:
+    line_month, line_date, line_time, line_info = line.split(" ",3)
+    line_time_hour, line_time_minute, line_time_second = line_time.split(":",2)
+    if line_time_hour == "07":
+        if int(line_time_minute) >= 0 and int(line_time_minute) <= 15:
+            count2 += 1
+print ("lines with timestamp between 07:00 and 07:15: ",count2)
+f.close()
 
 for count in range(1,16):
     if count % 3 == 0:
