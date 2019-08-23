1d34f2b2a6c4cd1031fd8a9d6dac787d2c1f22a8
maxkim <max@maxkim.com>
2019-08-23 16:11:14 -0700

    practice log file parsing, fizz buzz continued

--- maxtest.py
+++ maxtest.py
@@ -21,9 +21,8 @@ count2 = 0
 for line in f:
     line_month, line_date, line_time, line_info = line.split(" ",3)
     line_time_hour, line_time_minute, line_time_second = line_time.split(":",2)
-    if line_time_hour == "07":
-        if int(line_time_minute) >= 0 and int(line_time_minute) <= 15:
-            count2 += 1
+    if line_time_hour == "07" and 0 <= int(line_time_minute) <= 15:
+        count2 += 1
 print ("lines with timestamp between 07:00 and 07:15: ",count2)
 f.close()
 
